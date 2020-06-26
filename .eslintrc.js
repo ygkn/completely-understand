3a2de4264a883410011c72867ac01224a9f82a8d
Yugo Yagita <2000ygkn0713@gmail.com>
2020-06-27 04:05:05 +0900

    fix eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,16 @@ module.exports = {
     node: true,
   },
   extends: [
+    'airbnb-typescript',
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -18,7 +23,31 @@ module.exports = {
     },
     ecmaVersion: 2020,
     sourceType: 'module',
+    project: './tsconfig.json',
   },
   plugins: ['react', '@typescript-eslint'],
-  rules: { 'react/react-in-jsx-scope': 'off', 'react/prop-types': 'off' },
+  rules: {
+    'import/prefer-default-export': 'off',
+
+    // for Next.js
+    'react/react-in-jsx-scope': 'off',
+
+    // for TypeScript
+    'spaced-comment': ['error', 'always', { markers: ['/'] }],
+    'default-case': 'off',
+    'react/prop-types': 'off',
+
+    // restrict order of imports
+    'import/order': [
+      'error',
+      {
+        groups: [['builtin', 'external'], 'parent', ['sibling', 'index']],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
 };
