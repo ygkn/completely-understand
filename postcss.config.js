0406e62921fb9a4b91402eca28145580d5c7f61c
Yugo Yagita <2000ygkn0713@gmail.com>
2020-06-27 04:58:36 +0900

    fix purgecss setting

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 const purgecss = [
   '@fullhuman/postcss-purgecss',
   {
-    content: ['./components/**/*.tsx', './pages/**/*.tsx'],
+    content: ['./src/components/**/*.tsx', './src/pages/**/*.tsx'],
     defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
   },
 ];
